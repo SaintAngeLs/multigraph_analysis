6c1bb2f8bf198b2031fd33c5309f952412da7c41
olegkiprik <oleg.kiprik@proton.me>
2024-11-28 03:34:26 +0100

    (#2) Add a draft for the minimal extension algorithm

---
+++ tmp/all.hpp
@@ -0,0 +1,16 @@
+#ifndef INCL_ALL_H
+#define INCL_ALL_H
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+#include <map>
+#include <cstdint>
+#include <cstdlib>
+#include <cassert>
+#include <cstddef>
+#include <utility>
+#include <algorithm>
+
+#endif
