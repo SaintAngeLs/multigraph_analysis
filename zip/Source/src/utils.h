58a82d0605053cb9bce25b97a064c629a2da8976
olegkiprik <oleg.kiprik@proton.me>
2024-12-03 19:40:41 +0100

    Prepare zip file contents

---
+++ zip/Source/src/utils.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "graph.h" 
+
+int** allocate_matrix(int size);
+
+void free_matrix(int **matrix, int size);
+
+// Graph* load_graph_from_file(const char *filename);
+
+GraphInterface* load_multigraph_from_file(const char *filename);
+
+#endif
