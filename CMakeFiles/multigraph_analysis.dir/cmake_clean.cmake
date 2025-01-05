34dc3d177540df0fbc5642eeb9ed21a7d23ff6d3
an2508374 <an2508374@gmail.com>
2025-01-05 14:52:58 +0100

    (#26) add exe compiled on Microsoft Windows

---
+++ CMakeFiles/multigraph_analysis.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multigraph_analysis.dir/include/config.c.obj"
+  "CMakeFiles/multigraph_analysis.dir/include/config.c.obj.d"
+  "CMakeFiles/multigraph_analysis.dir/src/approximation_algorithm.c.obj"
+  "CMakeFiles/multigraph_analysis.dir/src/approximation_algorithm.c.obj.d"
+  "CMakeFiles/multigraph_analysis.dir/src/graph.c.obj"
+  "CMakeFiles/multigraph_analysis.dir/src/graph.c.obj.d"
+  "CMakeFiles/multigraph_analysis.dir/src/graph_algorithm.c.obj"
+  "CMakeFiles/multigraph_analysis.dir/src/graph_algorithm.c.obj.d"
+  "CMakeFiles/multigraph_analysis.dir/src/graph_factory.c.obj"
+  "CMakeFiles/multigraph_analysis.dir/src/graph_factory.c.obj.d"
+  "CMakeFiles/multigraph_analysis.dir/src/main.c.obj"
+  "CMakeFiles/multigraph_analysis.dir/src/main.c.obj.d"
+  "CMakeFiles/multigraph_analysis.dir/src/utils.c.obj"
+  "CMakeFiles/multigraph_analysis.dir/src/utils.c.obj.d"
+  "libmultigraph_analysis.dll.a"
+  "multigraph_analysis.exe"
+  "multigraph_analysis.exe.manifest"
+  "multigraph_analysis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/multigraph_analysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
