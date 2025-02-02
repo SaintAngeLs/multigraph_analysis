71d96950014d471a4fff199d88f743afc90ebd34
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2025-02-02 23:53:16 +0100

    (#18) update files structure

---
+++ vs_project/multigraph_analysis/src/method_wrapper.h
@@ -0,0 +1,61 @@
+#ifndef METHOD_WRAPPER_H
+#define METHOD_WRAPPER_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+#include "graph.h" 
+#include <windows.h>
+#include "utils.h"
+#include "graph_algorithm_context.h"
+#include "string_list.h"
+#include "cycle_list.h"
+#include "stack.h"
+#include "time.h"
+
+#define THRESHOLD     10  
+
+int calculate_size_wrapper(void* graph);
+
+int find_cycles_wrapper(
+    void* graph,
+    int vertices,
+    int*** output_cycles,
+    int** cycle_sizes,
+    int* cycle_count);
+
+int count_hamiltonian_cycles_wrapper(
+    void* graph,
+    int vertices,
+    int*** output_cycles,
+    int** cycle_sizes,
+    int* cycle_count);
+
+void calculate_metric_wrapper(
+    void* g1, int v1,
+    void* g2, int v2,
+    int* exact_metric,
+    int* approximate_metric);
+/*
+ * EXACT: find_minimal_extension_wrapper
+ */
+int find_minimal_extension_wrapper(void* graph, int vertices);
+/*
+ * EXACT: count_maximal_cycles_wrapper => length of the longest simple cycle
+ */
+int count_maximal_cycles_wrapper(void* graph, int vertices);
+
+/*
+ * EXACT: find_maximal_cycles_wrapper => enumerates all cycles that match
+ * the maximum cycle length found above.
+ */
+int find_maximal_cycles_wrapper(
+    void* graph,
+    int vertices,
+    int*** output_cycles,
+    int** cycle_sizes,
+    int* cycle_count
+);
+
+#endif /* METHOD_WRAPPER_H */
